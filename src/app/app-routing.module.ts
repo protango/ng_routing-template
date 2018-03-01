81c0b8d944904223f12ac0bd1c1d179e4f0b6e90
Zachary Murray <zacharymurray@hotmail.com>
2018-03-01 15:24:30 +1000

    initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './Index/Index.component';
+
+const routes: Routes = [
+  { path: 'index', component: IndexComponent },
+  { path: '', component: IndexComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { 
+
+}
